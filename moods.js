b1aea8d68df9f1fa50ff45c049f66f73ae80c249
Frances Berriman <fberriman@gmail.com>
2017-04-13 20:40:37 -0700

    Just more stubs

--- moods.js
+++ moods.js
@@ -1,11 +1,32 @@
-
 var moods = {
-  "shocked": "( 0 _ 0 )",
-  "glum": "( . _ . )",
-  "happy": "( ^ ᴗ ^ )",
-  "grumpy": "(｀ ⌒ ´ )",
-  "excited": "(  ˃ ᴗ ˂ )",
-  "sad": "( 0 ⌒ 0;)",
-  "depressed": "( - ⌒ -;)",
-  "sleeping": "(- ᴗ -)zZ"
+  "shocked": " 0 _ 0 ",
+  "glum": " . _ . ",
+  "happy": " ^ ᴗ ^ ",
+  "grumpy": "｀ ⌒ ´ ",
+  "excited": "  ˃ ᴗ ˂ ",
+  "sad": " 0 ⌒ 0;",
+  "depressed": " - ⌒ -; ",
+  "sleeping": " - ᴗ - zZ"
+};
+
+
+const wodehouse = document.getElementById("wodehouse");
+
+// setup page
+
+// connect to smartthings
+
+
+// check current status of house
+function getMood(){
+	var status = "happy";
+	return setMood(status)
 }
+
+// set appropriate face
+
+function setMood(status){
+	wodehouse.innerHTML = moods[status];
+}
+
+getMood(status)
