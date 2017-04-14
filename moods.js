a13278ec672fab523bf99e9e59fb88ad21d3b11e
Frances Berriman <fberriman@gmail.com>
2017-04-13 19:55:06 -0700

    Add stub for index and moods

---
+++ moods.js
@@ -0,0 +1,11 @@
+
+var moods = {
+  "shocked": "( 0 _ 0 )",
+  "glum": "( . _ . )",
+  "happy": "( ^ ᴗ ^ )",
+  "grumpy": "(｀ ⌒ ´ )",
+  "excited": "(  ˃ ᴗ ˂ )",
+  "sad": "( 0 ⌒ 0;)",
+  "depressed": "( - ⌒ -;)",
+  "sleeping": "(- ᴗ -)zZ"
+}
